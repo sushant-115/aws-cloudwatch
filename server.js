a9d8b492618f9edc391b28c97ce389ea05d48ebc
root <root@ip-172-31-81-18.ec2.internal>
2018-08-02 10:15:32 +0000

    cloud watch test

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const express = require("express");
+const app = express();
 const AWS = require("aws-sdk");
 AWS.config.update({region :"us-east-1"});
 const client = new AWS.CloudWatch();
+app.get("/" ,(req,res)=>{
 client.getDashboard({DashboardName : "demo"} ,(err,data)=>{
 console.log("err" ,err);
 console.log("data" ,data);
+res.send(data);
 })
+})
+app.listen(8001,err=>{console.log(8001)})
