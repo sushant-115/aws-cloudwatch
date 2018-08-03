fea57e11431d4cb969136bb11b1632d44da5d32d
Sushant <sushant@exotel.in>
2018-08-03 09:09:28 +0000

    getMetricData and listMetrics

--- server.js
+++ server.js
@@ -10,4 +10,51 @@ app.get("/", (req, res) => {
     res.send(data);
   })
 })
+const getParam=(dimension ,index)=>{
+var params = {
+  EndTime: new Date().toISOString() , /* required */
+  MetricDataQueries: [ /* required */
+    {
+      Id: 'm'+(index+1), /* required */
+      MetricStat: {
+        Metric: { /* required */
+          Dimensions: dimension.Dimensions,
+          MetricName: dimension.MetricName,
+          Namespace: dimension.Namespace
+        },
+        Period:3000, /* required */
+        Stat: 'Average',
+        Unit :'Seconds'
+      },
+      ReturnData: true 
+    },
+    /* more items */
+  ],
+  StartTime: new Date(Date.now() -1000*60*60*24*4).toISOString() ,
+  MaxDatapoints: 1000
+};
+return params;
+}
+const listParams = {
+  Dimensions: [
+    {
+      Name: 'InstanceId', /* required */
+      Value: 'i-0cf7e79edc30e1620'
+    },
+    /* more items */
+  ],
+//  MetricName: 'CPUUtilization',
+  Namespace :'AWS/EC2'
+}
+app.get("/metric" , (req,res)=>{
+  client.listMetrics(listParams ,(err, data)=>{
+    console.log("err" ,err);
+//    console.log("data" ,data);
+    const metricListParams = data.Metrics.map(getParam);
+	const arr = [];
+    metricListParams.forEach(d=>client.getMetricData(d,(err,data)=>arr.push(data)));
+	setTimeout(()=>res.send(arr) ,5000);
+  })
+})
+
 app.listen(8001, err => { console.log(8001) })
